83ac3cf8f7409fd05a41c006671c6bae80337bd2
Prime Dev <prime.dev22@gmail.com>
2021-01-16 00:44:10 +0900

    Clear unnecessary files Create empty Sheet component Set colors as primary, secondary and accent

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,23 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app color="primary" dark>
+      <h2 class="d-flex align-center">My Worksheet Maker</h2>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <Sheet />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Sheet from './components/Sheet';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Sheet,
   },
 
   data: () => ({
